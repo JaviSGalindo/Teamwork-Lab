921347d975bdb6340ba4ca9620cce9ee1db7a682
Andry <andry.carolina88@gmail.com>
2024-12-19 14:36:10 -0500

    function script colorAleatory

--- ad-l3-1.0/script.js
+++ ad-l3-1.0/script.js
@@ -1,11 +1,12 @@
-function cambiarColor() {
-  const colores = ['green', 'blue', 'red'];
-  const elementosH5 = document.querySelectorAll('h5');
+function changingColor() {
+  const colors = ['green', 'blue', 'red'];
+  const elements = document.querySelectorAll('h5');
 
-  elementosH5.forEach(h5 => {
+  elements.forEach(h5 => {
     h5.addEventListener('click', () => {
-      const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
-      h5.style.backgroundColor = colorAleatorio;
+      const colorAleatory = colors[Math.floor(Math.random() * colors.length)];
+      h5.style.backgroundColor = colorAleatory
     });
   });
 }
+changingColor()
